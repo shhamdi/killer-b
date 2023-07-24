87bc6f25a86effebd6ebea6fb60d82251d9d35fa
shhamdi <sameh.hamdi73@gmail.com>
2023-07-24 10:24:00 +0100

    feat: add site footer and tailwind indicator for screen's size

---
+++ src/components/site-footer.tsx
@@ -0,0 +1,42 @@
+import { Icons } from "./icons"
+
+const SiteFooter = ({ className }: React.HTMLAttributes<HTMLElement>) => {
+  return (
+    <footer className={className}>
+      <div className="container flex flex-col items-center gap-5 py-8 md:flex-row">
+        <Icons.logo className="mr-2 h-7 w-7" />
+        <p className="text-center text-sm leading-loose md:text-left">
+          Built by{" "}
+          <a
+            href="https://github.com/shhamdi"
+            target="_blank"
+            rel="noreferrer"
+            className="font-semibold underline"
+          >
+            shhamdi
+          </a>
+          . Hosted on{" "}
+          <a
+            href="https://vercel.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="font-semibold underline"
+          >
+            Vercel
+          </a>
+          . The source code is available on{" "}
+          <a
+            href="https://github.com/shhamdi/killer-b"
+            target="_blank"
+            rel="noreferrer"
+            className="font-semibold underline"
+          >
+            Github
+          </a>
+        </p>
+      </div>
+    </footer>
+  )
+}
+
+export default SiteFooter
