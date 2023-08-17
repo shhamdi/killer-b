f3e48021a8e6a0d59f2e73f06c97a0564536dd9f
shhamdi <sameh.hamdi73@gmail.com>
2023-08-17 22:30:15 +0100

    fix: fix the first rendering value of the dark/light mode switch

--- src/components/ui/custom-switch.tsx
+++ src/components/ui/custom-switch.tsx
@@ -1,7 +1,6 @@
 "use client"
 
-import * as React from "react"
-import { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { cn } from "@/utils/classname"
 import * as SwitchPrimitives from "@radix-ui/react-switch"
 import { useTheme } from "next-themes"
@@ -13,33 +12,40 @@ const ToggleTheme = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>
 >(({ className, ...props }, ref) => {
   const { theme, setTheme } = useTheme()
+  const [isClient, setIsClient] = useState<boolean>(false)
 
-  useEffect(() => {}, [theme])
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
 
   return (
-    <SwitchPrimitives.Root
-      aria-label="dark-mode-toggle"
-      className={cn(
-        "relative",
-        "peer inline-flex h-5 w-[43px] shrink-0 cursor-pointer items-center rounded-full transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
-        "border border-gray-400 data-[state=checked]:bg-accent",
-        className
-      )}
-      {...props}
-      ref={ref}
-      defaultChecked={theme === "dark"}
-      onCheckedChange={(checked) =>
-        checked ? setTheme("dark") : setTheme("light")
-      }
-    >
-      <Icons.moon className="absolute left-0 h-4 w-4" />
-      <Icons.sun className="absolute right-0 h-4 w-4" />
-      <SwitchPrimitives.Thumb
-        className={cn(
-          "pointer-events-none block h-4 w-4 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-6 data-[state=unchecked]:translate-x-0 dark:bg-secondary-border"
-        )}
-      />
-    </SwitchPrimitives.Root>
+    isClient && (
+      <div className="flex items-center gap-2">
+        <SwitchPrimitives.Root
+          aria-label="dark-mode-toggle"
+          className={cn(
+            "relative",
+            "peer inline-flex h-5 w-[43px] shrink-0 cursor-pointer items-center rounded-full transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
+            "border border-gray-400 data-[state=checked]:bg-accent",
+            className
+          )}
+          {...props}
+          ref={ref}
+          defaultChecked={theme === "dark"}
+          onCheckedChange={(checked) =>
+            checked ? setTheme("dark") : setTheme("light")
+          }
+        >
+          <Icons.moon className="absolute left-0 h-4 w-4" />
+          <Icons.sun className="absolute right-0 h-4 w-4" />
+          <SwitchPrimitives.Thumb
+            className={cn(
+              "pointer-events-none block h-4 w-4 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-6 data-[state=unchecked]:translate-x-0 dark:bg-secondary-border"
+            )}
+          />
+        </SwitchPrimitives.Root>
+      </div>
+    )
   )
 })
 ToggleTheme.displayName = SwitchPrimitives.Root.displayName
