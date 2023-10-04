e2caad3e246a05eb1721828604868f77b217ac28
shhamdi <sameh.hamdi73@gmail.com>
2023-10-04 14:44:59 +0100

    fix: resolve opening of context menu inside sheet component

--- src/components/mobile-dashboard-sidebar.tsx
+++ src/components/mobile-dashboard-sidebar.tsx
@@ -5,7 +5,7 @@ import { useState } from "react"
 import { Icons } from "./icons"
 import MainLogo from "./main-logo"
 import { Button } from "./ui/button"
-import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet"
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./ui/sheet"
 
 interface MobileDashboardSidebarProps {
   children: React.ReactNode
@@ -15,7 +15,7 @@ const MobileDashboardSidebar = ({ children }: MobileDashboardSidebarProps) => {
   const [open, setOpen] = useState(false)
 
   return (
-    <Sheet open={open} onOpenChange={setOpen}>
+    <Sheet open={open} onOpenChange={setOpen} modal={false}>
       <SheetTrigger asChild>
         <Button variant="ghost" className="m-0 p-0 md:hidden">
           <Icons.mobileTrigger className="h-5 w-5" />
@@ -23,7 +23,9 @@ const MobileDashboardSidebar = ({ children }: MobileDashboardSidebarProps) => {
         </Button>
       </SheetTrigger>
       <SheetContent side="left">
-        <MainLogo className="flex" />
+        <SheetHeader>
+          <MainLogo className="flex" />
+        </SheetHeader>
         {children}
       </SheetContent>
     </Sheet>
